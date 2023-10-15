6e6cb9d2b88dd25444287401c7e95786afc5648b
sarebok <bortonae@hotmail.com>
2023-10-15 14:24:20 -0300

    create main components

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+/* El componente formulario debe contener el formulario con los campos necesarios
+para agregar colaboradores a la lista y un botón para agregar los campos, además se
+debe validar que los campos no se encuentren vacíos */
+
+import React from "react";
+
+const Formulario = () => {
+  return <div>Formulario</div>;
+};
+
+export default Formulario;
---
+++ src/components/Listado.jsx
@@ -0,0 +1,10 @@
+/*  En el componente Listado se deben mostrar los datos de los colaboradores
+utilizando una tabla de Bootstrap. */
+
+import React from "react";
+
+const Listado = () => {
+  return <div>Listado</div>;
+};
+
+export default Listado;
